bd925664c66b5b61eb724933b849b8d472272e68
Balavishnu V J <balavishnuvj@Balavishnus-MacBook-Pro.local>
2019-02-07 23:22:13 +0530

    Adds: Highlighting Ingredients

--- scripts/content.js
+++ scripts/content.js
@@ -1,64 +1,61 @@
-console.log('content script has loaded')
+console.log("content script has loaded");
 
-
-
-
-chrome.runtime.onMessage.addListener(function (msg, sender, response) {
+chrome.runtime.onMessage.addListener(function(msg, sender, response) {
   // First, validate the message's structure
-  console.log('i listented')
-  if ((msg.from === 'popup') && (msg.subject === 'search')) {
-      const message = msg.message;
-      console.log(message);
-      message.forEach(item=>{
-        searchInDom(item);
-      });
-    response('hey');
+  console.log("i listented");
+  if (msg.from === "popup" && msg.subject === "search") {
+    const message = msg.message;
+    console.log(message);
+    message.forEach(item => {
+      searchInDom(item);
+    });
+    highlighTerms(message);
+    response("hey");
   }
 });
 
-function searchInDom(searchTerms){
-  const nodeTypesIgnored = ['DIV','ARTICLE','SECTION','HEADER','FOOTER','IMG', 'SCRIPT','BODY', 'HEAD','UL', 'BR', 'NAV','FORM','INPUT' ,'svg','path', 'polygon','MAIN'];
-  const className = makeid();
-  $('*', 'body')
-  .addBack()
-  .contents()
-  .filter(function(){
-      const isValid = !nodeTypesIgnored.includes(this.nodeName);
-      return isValid
-  })
-  .filter(function(){
-      const isValid = (!!(this.nodeValue && isInString(this.nodeValue,searchTerms))) || (!!(this.innerText && isInString(this.innerText,searchTerms)));
-      return isValid
-  })
-  .each(function(){
-      var elem = this.nodeValue !==null ? $(this).parent() : $(this); 
-      elem.addClass(className)
-  });
-  
-  $('#dynamicStyle').remove();
-
-  const style = $('<style/>').attr('id','dynamicStyle').text(
-    `
+function highlighTerms(matchedIngs) {
+  var className = "marked";
+  var style = $("<style/>")
+    .attr("id", "dynamicStyle")
+    .text(
+      `
     .${className},  .${className} * {
       background-color: #3f51b5 !important;
       color:white !important;
     }
     `
-  )
-  $('head').append(style);
-
+    );
+  $("head").append(style);
+  var elementsToBeParsed = ["ul", "table"];
+  elementsToBeParsed.forEach(el => {
+    $(el).each(function() {
+      const elm = this;
+      matchedIngs.forEach(ings => {
+        if (!$(elm).hasClass("marked")) {
+          var pattern = new RegExp(ings, "g");
+          var newhtml = $(elm)
+            .html()
+            .replace(pattern, '<span class="marked">' + ings + "</span>");
+          $(elm).html(newhtml);
+        }
+      });
+    });
+  });
 }
-function isInString(str,term){
+
+function isInString(str, term) {
   return lowerCaseTrimmed(str).indexOf(term) !== -1;
 }
 
-function lowerCaseTrimmed(str){
+function lowerCaseTrimmed(str) {
   return str.toLowerCase().trim();
 }
 
 function makeid() {
   var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (var i = 0; i < 5; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
